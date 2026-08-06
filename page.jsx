c512a0794ff9c4b9131c6b6fcc8006096b64b534
MeetAaron <aaron@meetaaron.app>
2026-08-06 17:15:50 +0800

    Delete page.jsx

--- page.jsx
+++
@@ -1,9 +0,0 @@
-import MeetAaronPrototype from "../../components/MeetAaronPrototype";
-
-export default function AppPage() {
-  return (
-    <main style={{ minHeight: "100vh", padding: "16px", background: "#F6F4EF" }}>
-      <MeetAaronPrototype />
-    </main>
-  );
-}
